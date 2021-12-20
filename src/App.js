7a972c75a37ec51ee2896c357b19bc0596515853
mohamed18yo <fack21gasa@gmail.com>
2021-12-20 16:06:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Button,
+  Form,
+  Checkbox,
+  Input,
+  Select,
+  Typography,
+  Row,
+  Col,
+} from "antd";
+import "antd/dist/antd.css";
 
+const { TextArea } = Input;
 function App() {
+  const { Option } = Select;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="contianer">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          style={{ width: "50%", margin: "auto" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Row style={{ justifyContent: "space-between" }}>
+            <Form.Item
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+              label="Field"
+              name="Field"
+            ></Form.Item>
+            <Select
+              defaultValue="lucy"
+              style={{ width: "60% " }}
+              onChange={() => {}}
+            >
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+              <Option value="disabled" disabled>
+                Disabled
+              </Option>
+              <Option value="Yiminghe">yiminghe</Option>
+            </Select>
+          </Row>
+
+          <Typography.Text style={{ marginBottom: "1rem" }}>
+            From: Iv_alldata_for_statment
+          </Typography.Text>
+          <Row style={{ alignItems: "center" }}>
+            <Form.Item
+              label="operation"
+              name="checked"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+            >
+              <Checkbox
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                }}
+                label="test"
+                onChange={() => ``}
+              ></Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                width: "50%",
+              }}
+            >
+              <Select
+                defaultValue="lucy"
+                style={{ width: "80% " }}
+                onChange={() => {}}
+              >
+                <Option value="jack">Is_Equle</Option>
+                <Option value="lucy">Is_Equle</Option>
+              </Select>
+            </Form.Item>
+
+            <Checkbox
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+              label="test"
+              onChange={() => ``}
+            ></Checkbox>
+            <Form.Item
+              label="Ignor Case"
+              name="Ignor Case"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+            ></Form.Item>
+          </Row>
+          <Row>
+            <Form.Item
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+              label="value"
+              name="value"
+            ></Form.Item>
+            <Input style={{ width: "60%" }} />
+          </Row>
+
+          <Row width="100%" style={{ justifyContent: "space-between" }}>
+            <Typography.Text style={{ marginBottom: "1rem" }}>
+              Condidation Spcefied
+            </Typography.Text>
+            <Typography.Text style={{ marginBottom: "1rem" }}>
+              Ignor Case
+            </Typography.Text>
+          </Row>
+          <TextArea
+            showCount
+            maxLength={100}
+            style={{ height: 120 }}
+            onChange={() => ``}
+          />
+          <Row justify="space-between" style={{ marginTop: "1rem" }} align="">
+            <Col span={4}>
+              <Button type="primary">Save</Button>
+            </Col>
+            <Col span={4}>
+              <Button type="primary">Restore</Button>
+            </Col>
+            <Col span={4}>
+              <Button type="primary">Ok</Button>
+            </Col>
+            <Col span={4}>
+              <Button type="primary">Cancel</Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
     </div>
   );
 }
